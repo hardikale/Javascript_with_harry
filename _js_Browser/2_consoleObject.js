365cfc34c03c9f5e1e80551bc7141c4f829eb42b
hardikale <mehardikale@gmail.com>
2025-08-02 10:06:26 +0545

    console object methods

---
+++ _js_Browser/2_consoleObject.js
@@ -0,0 +1,14 @@
+console.log("hello people!");
+console.error("this is and error.");
+
+console.assert(1>5);
+console.assert(5>1);
+
+console.log("console.clear() will clear the screen.")
+
+obj = {a:2, b:3 ,c:4, d:5}
+console.table(obj)
+
+console.warn("This is a warning.")
+
+console.info("this is and info. similar to console.log")
