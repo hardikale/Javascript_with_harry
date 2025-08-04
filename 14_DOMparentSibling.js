a1ac29a7a790332f1136af183cb7460f6b392062
hardikale <mehardikale@gmail.com>
2025-08-04 22:37:06 +0545

    DOM parent and sibling

---
+++ 14_DOMparentSibling.js
@@ -0,0 +1,6 @@
+console.log(document.body.firstElementChild)
+ 
+const a= document.body.firstElementChild
+console.log(a.parentElement)
+console.log(a.nextElementSibling)
+console.log(a.nextElementSibling.firstElementChild)
